d2be8b22dbb02f5a4e6a8d2673a3dd511b96ea4f
anilemrah <emrah.anl@gmail.com>
2017-03-26 19:11:40 +0300

    Language changed

--- Calculator/src/calcAPP.java
+++ Calculator/src/calcAPP.java
@@ -8,15 +8,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
 
+//Java example Anil Emrah
+//Basic Calculator
+
 public class calcAPP {
 
-	private JFrame frmCalculatorapp;
+	private JFrame calculatorapp;
 	private JTextField textField;
-	double sayi1;
-	double sayi2;
-	String sonuc;
-	String islem;
-
+	double num1;
+	double num2;
+	String result;
+	String operation;
 	/**
 	 * Launch the application.
 	 */
@@ -25,7 +27,7 @@ public class calcAPP {
 			public void run() {
 				try {
 					calcAPP window = new calcAPP();
-					window.frmCalculatorapp.setVisible(true);
+					window.calculatorapp.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -39,273 +41,289 @@ public class calcAPP {
 	public calcAPP() {
 		initialize();
 	}
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmCalculatorapp = new JFrame();
-		frmCalculatorapp.setTitle("CalculatorAPP");
-		frmCalculatorapp.setBounds(100, 100, 250, 392);
-		frmCalculatorapp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCalculatorapp.getContentPane().setLayout(null);
+		calculatorapp = new JFrame();
+		calculatorapp.setTitle("CalcAPP");
+		calculatorapp.setBounds(100, 100, 250, 392);
+		calculatorapp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		calculatorapp.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
 		textField.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField.setBounds(10, 10, 214, 30);
-		frmCalculatorapp.getContentPane().add(textField);
+		calculatorapp.getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		JButton buton7 = new JButton("7");
-		buton7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		//Button 7 starts here
+		JButton button7 = new JButton("7");
+		button7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button7.getText(); //it tooks what textfield already has and adds what you clicked
+				textField.setText(x); //shows what you have finally
 				
-				String x = textField.getText() + buton7.getText();
+			}
+		});
+		button7.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button7.setBounds(10, 51, 50, 50);
+		calculatorapp.getContentPane().add(button7);
+		//Button 7 finished here
+		//Button 9 starts here
+		JButton button8 = new JButton("8");
+		button8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button8.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton7.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton7.setBounds(10, 51, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton7);
-		
-		JButton buton8 = new JButton("8");
-		buton8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton8.getText();
+		button8.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button8.setBounds(65, 51, 50, 50);
+		calculatorapp.getContentPane().add(button8);
+		//Button8 finished here
+		//Button9 starts here
+		JButton button9 = new JButton("9");
+		button9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button9.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton8.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton8.setBounds(65, 51, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton8);
-		
-		JButton buton9 = new JButton("9");
-		buton9.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton9.getText();
-				textField.setText(x);
-				
-			}
-		});
-		buton9.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton9.setBounds(120, 51, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton9);
-		
-		JButton butonbolu = new JButton("/");
-		butonbolu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				islem = "/";
-				sayi1=Double.parseDouble(textField.getText());
+		button9.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button9.setBounds(120, 51, 50, 50);
+		calculatorapp.getContentPane().add(button9);
+		//Button9 finished here
+		//Buttondivide starts here
+		JButton buttondivide = new JButton("/");
+		buttondivide.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				operation = "/";
+				num1=Double.parseDouble(textField.getText());
 				textField.setText("");
 				
 			}
 		});
 		
-		butonbolu.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonbolu.setBounds(175, 51, 50, 50);
-		frmCalculatorapp.getContentPane().add(butonbolu);
-		
-		JButton buton4 = new JButton("4");
-		buton4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton4.getText();
+		buttondivide.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttondivide.setBounds(175, 51, 50, 50);
+		calculatorapp.getContentPane().add(buttondivide);
+		//Buttondivide finished here
+		//Button4 starts here
+		JButton button4 = new JButton("4");
+		button4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button4.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton4.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton4.setBounds(10, 112, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton4);
-		
-		JButton buton5 = new JButton("5");
-		buton5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton5.getText();
+		button4.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button4.setBounds(10, 112, 50, 50);
+		calculatorapp.getContentPane().add(button4);
+		//Button4 finished here
+		//Button5 starts here
+		JButton button5 = new JButton("5");
+		button5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button5.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton5.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton5.setBounds(65, 112, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton5);
-		
-		JButton buton6 = new JButton("6");
-		buton6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton6.getText();
+		button5.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button5.setBounds(65, 112, 50, 50);
+		calculatorapp.getContentPane().add(button5);
+		//Button5 finished here
+		//Button6 starts here
+		JButton button6 = new JButton("6");
+		button6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button6.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton6.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton6.setBounds(120, 112, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton6);
-		
-		JButton butonx = new JButton("x");
-		butonx.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				islem ="x";
-				sayi1=Double.parseDouble(textField.getText());
+		button6.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button6.setBounds(120, 112, 50, 50);
+		calculatorapp.getContentPane().add(button6);
+		//Button6 finished here
+		//Buttonx starts here
+		JButton buttonx = new JButton("x");
+		buttonx.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				operation ="x";
+				num1=Double.parseDouble(textField.getText());
 				textField.setText("");
 				
 			}
 		});
-		butonx.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonx.setBounds(175, 112, 50, 50);
-		frmCalculatorapp.getContentPane().add(butonx);
-		
-		JButton buton1 = new JButton("1");
-		buton1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton1.getText();
+		buttonx.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttonx.setBounds(175, 112, 50, 50);
+		calculatorapp.getContentPane().add(buttonx);
+		//Buttonx finished here
+		//Button1 starts here
+		JButton button1 = new JButton("1");
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button1.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton1.setBounds(10, 173, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton1);
-		
-		JButton buton2 = new JButton("2");
-		buton2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton2.getText();
+		button1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button1.setBounds(10, 173, 50, 50);
+		calculatorapp.getContentPane().add(button1);
+		//Button1 finished here
+		//Button2 starts here
+		JButton button2 = new JButton("2");
+		button2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button2.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton2.setBounds(65, 173, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton2);
-		
-		JButton buton3 = new JButton("3");
-		buton3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton3.getText();
+		button2.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button2.setBounds(65, 173, 50, 50);
+		calculatorapp.getContentPane().add(button2);
+		//Button2 finished here
+		//Button3 starts here
+		JButton button3 = new JButton("3");
+		button3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button3.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton3.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton3.setBounds(120, 173, 50, 50);
-		frmCalculatorapp.getContentPane().add(buton3);
-		
-		JButton butoneksi = new JButton("-");
-		butoneksi.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				islem = "-";
-				sayi1=Double.parseDouble(textField.getText());
+		button3.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button3.setBounds(120, 173, 50, 50);
+		calculatorapp.getContentPane().add(button3);
+		//Button3 finished here
+		//Buttonminus starts here
+		JButton buttonminus = new JButton("-");
+		buttonminus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				operation = "-";
+				num1=Double.parseDouble(textField.getText());
 				textField.setText("");
 				
 			}
 		});
-		butoneksi.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butoneksi.setBounds(175, 173, 50, 50);
-		frmCalculatorapp.getContentPane().add(butoneksi);
-		
-		JButton buton0 = new JButton("0");
-		buton0.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String x = textField.getText() + buton0.getText();
+		buttonminus.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttonminus.setBounds(175, 173, 50, 50);
+		calculatorapp.getContentPane().add(buttonminus);
+		//Buttonminus finished here
+		//Button0 starts here
+		JButton button0 = new JButton("0");
+		button0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				String x = textField.getText() + button0.getText();
 				textField.setText(x);
 				
 			}
 		});
-		buton0.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		buton0.setBounds(9, 234, 106, 50);
-		frmCalculatorapp.getContentPane().add(buton0);
-		
-		JButton butonnokta = new JButton(".");
-		butonnokta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		button0.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		button0.setBounds(9, 234, 106, 50);
+		calculatorapp.getContentPane().add(button0);
+		//Button0 finished here
+		//Buttondot starts here
+		JButton buttondot = new JButton(".");
+		buttondot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				
-				String x = textField.getText() + butonnokta.getText();
+				String x = textField.getText() + buttondot.getText();
 				textField.setText(x);
 				
 			}
 		});
-		butonnokta.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonnokta.setBounds(119, 234, 50, 50);
-		frmCalculatorapp.getContentPane().add(butonnokta);
-		
-		JButton butonarti = new JButton("+");
-		butonarti.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		buttondot.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttondot.setBounds(119, 234, 50, 50);
+		calculatorapp.getContentPane().add(buttondot);
+		//Buttondot finished here
+		//Buttonplus starts here
+		JButton buttonplus = new JButton("+");
+		buttonplus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				
-				islem = "+";
-				sayi1=Double.parseDouble(textField.getText());
+				operation = "+";
+				num1=Double.parseDouble(textField.getText());
 				textField.setText("");
 				
 			}
 		});
-		butonarti.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonarti.setBounds(174, 234, 50, 50);
-		frmCalculatorapp.getContentPane().add(butonarti);
-		
-		JButton butonesit = new JButton("=");
-		
-		
-		butonesit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				if(islem=="x"){
+		buttonplus.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttonplus.setBounds(174, 234, 50, 50);
+		calculatorapp.getContentPane().add(buttonplus);
+		//Buttonplus finished here
+		//Buttonequal starts here
+		JButton buttonequal = new JButton("=");
+		buttonequal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
+				//Checks what operation you are doing
+				if(operation=="x"){
 
-				sayi2=Double.parseDouble(textField.getText());
-				sonuc=String.format("%.2f",sayi1*sayi2);
-				textField.setText(sonuc);
+				num2=Double.parseDouble(textField.getText());
+				result=String.format("%.2f",num1*num2);
+				textField.setText(result);
+				
 				}
-				else if(islem=="-"){
+				else if(operation=="-"){
 
-					sayi2=Double.parseDouble(textField.getText());
-					sonuc=String.format("%.2f",sayi1-sayi2);
-					textField.setText(sonuc);
+					num2=Double.parseDouble(textField.getText());
+					result=String.format("%.2f",num1-num2);
+					textField.setText(result);
+					
 					}
-				else if(islem=="+"){
+				else if(operation=="+"){
 
-					sayi2=Double.parseDouble(textField.getText());
-					sonuc=String.format("%.2f",sayi1+sayi2);
-					textField.setText(sonuc);
+					num2=Double.parseDouble(textField.getText());
+					result=String.format("%.2f",num1+num2);
+					textField.setText(result);
+					
 					}
-				else if(islem=="/"){
+				else if(operation=="/"){
 
-					sayi2=Double.parseDouble(textField.getText());
-					sonuc=String.format("%.2f",sayi1/sayi2);
-					textField.setText(sonuc);
+					num2=Double.parseDouble(textField.getText());
+					result=String.format("%.2f",num1/num2);
+					textField.setText(result);
+					
 					}
 			}
 		});
 		
-		
-		
-		butonesit.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonesit.setBounds(10, 292, 105, 50);
-		frmCalculatorapp.getContentPane().add(butonesit);
-		
-		JButton butonC = new JButton("C");
-		butonC.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				
+		buttonequal.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttonequal.setBounds(10, 292, 105, 50);
+		calculatorapp.getContentPane().add(buttonequal);
+		//Buttonequal finished here
+		//ButtonC starts here
+		JButton buttonC = new JButton("C");
+		buttonC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//What you want to do when this button clicked
 				textField.setText(null);
 				
 			}
 		});
-		butonC.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		butonC.setBounds(119, 292, 105, 50);
-		frmCalculatorapp.getContentPane().add(butonC);
+		buttonC.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		buttonC.setBounds(119, 292, 105, 50);
+		calculatorapp.getContentPane().add(buttonC);
+		//ButtonC finished here
 	}
 }
